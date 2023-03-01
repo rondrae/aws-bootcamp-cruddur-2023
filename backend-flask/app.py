1be80fd3445099b1553c56c6bd4b3c3c095604f4
rondrae <rondrae@gmail.com>
2023-03-01 13:13:08 -0500

    added cloudwatch logs

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -18,6 +18,22 @@ from services.show_activity import *
 from aws_xray_sdk.core import xray_recorder
 from aws_xray_sdk.ext.flask.middleware import XRayMiddleware
 
+# Cloud Watch logs---------------------------------------------------
+import watchtower
+import logging
+from time import strftime
+
+
+# Configuring Logger to Use CloudWatch
+LOGGER = logging.getLogger(__name__)
+LOGGER.setLevel(logging.DEBUG)
+console_handler = logging.StreamHandler()
+cw_handler = watchtower.CloudWatchLogHandler(log_group='cruddur')
+LOGGER.addHandler(console_handler)
+LOGGER.addHandler(cw_handler)
+LOGGER.info ("test log")
+
+
 xray_url = os.getenv("AWS_XRAY_URL")
 xray_recorder.configure(service='backend-flask', dynamic_naming=xray_url)
 
@@ -43,6 +59,12 @@ def data_message_groups():
     return model['errors'], 422
   else:
     return model['data'], 200
+
+@app.after_request
+def after_request(response):
+    timestamp = strftime('[%Y-%b-%d %H:%M]')
+    LOGGER.error('%s %s %s %s %s %s', timestamp, request.remote_addr, request.method, request.scheme, request.full_path, response.status)
+    return response
 
 @app.route("/api/messages/@<string:handle>", methods=['GET'])
 def data_messages(handle):
@@ -72,7 +94,7 @@ def data_create_message():
 
 @app.route("/api/activities/home", methods=['GET'])
 def data_home():
-  data = HomeActivities.run()
+  data = HomeActivities.run(logger= LOGGER)
   return data, 200
 
 @app.route("/api/activities/notifications", methods=['GET'])
